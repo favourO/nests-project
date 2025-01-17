bb0f0cdd698e47d8e6bbb7028636a9f97d51c160
Favour Ojiaku <ojiakufavour@gmail.com>
2025-01-17 15:13:50 +0000

    Nest Project

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Request } from "express";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signup (@Body() dto: AuthDto) {
+        return this.authService.signup(dto);
+    }
+
+    @Post('signin')
+    signin () {
+        return "Signin";
+    }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService) {}
+    async signup (dto: AuthDto) {
+        // generate the password hash
+        const hash = await argon.hash(dto.password);
+
+        // save new user in the db
+        const user = await this.prisma.users.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+            // select: {
+            //     id: true,
+            //     email: true,
+            //     createdAt: true
+            // }
+        });
+        delete user.hash
+        // return the saved user
+        return user;
+    }
+
+    signin () {
+
+    }
+}
